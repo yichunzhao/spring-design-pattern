93815206e2f607f312d2c1f61c230b9b915834e8
yichunzhao <zhaoyichun@hotmail.com>
2020-06-30 04:29:21 +0200

    reformatting comment

--- src/main/java/com/ynz/springdesignpattern/decorator/DemoRifleAndDecorators.java
+++ src/main/java/com/ynz/springdesignpattern/decorator/DemoRifleAndDecorators.java
@@ -1,9 +1,15 @@
 package com.ynz.springdesignpattern.decorator;
 
+/**
+ * Add a new functionality to an existing instance in a runtime.
+ */
 public class DemoRifleAndDecorators {
 
     public static void main(String[] args) {
         Rifle rifle = new Rifle();
+        rifle.shoot();
+
+        System.out.println("++++++++++++++++++++++");
 
         LaserGenerator laserDecoratedRifle = new LaserGenerator(rifle);
         laserDecoratedRifle.launch();
